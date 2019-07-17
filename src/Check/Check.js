782e7cdc2ddae5bd413fae138a57542df791395a
James Bliss <james.bliss@lush.co.uk>
2019-07-17 16:03:04 +0100

    Fixing build

--- src/Check/Check.js
+++ src/Check/Check.js
@@ -37,7 +37,7 @@ const InputField = ({
 
   return (
     <>
-      <input { ...props } />
+      <input id={ fieldName } { ...props } />
 
       { label && <label htmlFor={ fieldName }>{ label }</label> }
 
