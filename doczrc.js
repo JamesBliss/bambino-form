b41b40f73b4e57864dafa08f0df7f9c9aa0f3040
James Bliss <james.bliss@lush.co.uk>
2019-08-02 10:09:43 +0100

    attempt to deploy docz

---
+++ doczrc.js
@@ -0,0 +1,11 @@
+// export default {
+//   dest: '/docs/playground',
+//   hashRouter: true,
+//   base: '/playground'
+// };
+
+module.exports = {
+  dest: '/docs/playground',
+  hashRouter: true,
+  base: '/playground'
+};
